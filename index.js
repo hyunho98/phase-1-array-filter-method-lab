744eff53286fd7886fa557f1b53cc44cf36bfd54
Alex Lee <hyunho98@gmail.com>
2022-10-30 19:09:31 -0400

    commit tree d3603a0c071da87d98677ecac36af7d3e1f291e2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function(driver) {
+        for (let i = 0; i < name.length; i++) {
+            if (driver[i].toUpperCase() !== name[i].toUpperCase())
+                return false;
+        }
+        return true;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toUpperCase() === name.toUpperCase());
+}
+
